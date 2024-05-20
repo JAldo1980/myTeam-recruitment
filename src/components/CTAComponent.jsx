98f98046ecfff081a250b7c230fa432f91facca2
JAldo1980 <jamesalderman1980@gmail.com>
2024-05-20 13:32:05 +0100

    update start component

--- src/components/CTAComponent.jsx
+++ src/components/CTAComponent.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import { NavLink } from "react-router-dom";
 
 const CTAComponent = () => {
   return (
@@ -8,11 +9,17 @@ const CTAComponent = () => {
         <h3 className="text-3xl sm:text-5xl font-bold">
           Ready to get started?
         </h3>
-        <Button
-          type="button"
-          className="mt-4 sm:mt-0 border-black rounded-full border-2 py-2 px-6 text-black cursor-pointer hover:text-white hover:bg-black duration-300"
-          label="Contact Us"
-        />
+
+        <NavLink
+          to="/about"
+          className="text-white cursor-pointer hover-border-transition"
+        >
+          <Button
+            type="button"
+            className="mt-4 sm:mt-0 border-black rounded-full border-2 py-2 px-6 text-black cursor-pointer hover:text-white hover:bg-black duration-300"
+            label="Contact Us"
+          />
+        </NavLink>
       </div>
     </div>
   );

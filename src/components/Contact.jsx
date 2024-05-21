ce83610c22aa1ec66568900098c36a3a88454b2f
JAldo1980 <jamesalderman1980@gmail.com>
2024-05-21 10:55:55 +0100

    contact from update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,11 +2,7 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div
-      className="text-container grid 
-      grid-cols-1 gap-6 pb-60
-      sm:grid-cols-2 sm:pb-40 sm:gap-12"
-    >
+    <div className="text-container pb-60">
       <div>
         <h1 className="text-4xl livvic-semibold sm:text-start sm:text-7xl text-white font-bold">
           Contact
@@ -14,11 +10,41 @@ const Contact = () => {
         <p className="mt-4 text-white">
           Get in contact with us today to discuss how we can help you find the
           best talent!
+          <br></br>
+          <span className="custom-color text-2xl font-bold">Ask us about:</span>
         </p>
       </div>
 
-      <div className="grid">
-        <div className=" p-8 rounded-lg shadow-lg w-full max-w-md">
+      <div className="grid mt-8 sm:grid-cols-2">
+        {/*  */}
+        <div className="flex flex-col gap-10 mt-10">
+          <div className="flex gap-6 items-center">
+            <img src="icon-person.svg" alt="person-icon" />
+            <div className="flex flex-col">
+              <h4 className="custom-color font-bold text-xl">
+                The quality of our talent network
+              </h4>
+            </div>
+          </div>
+          <div className="flex gap-6 items-center">
+            <img src="icon-cog.svg" alt="cog-icon" />
+            <div className="flex flex-col">
+              <h4 className="custom-color font-bold text-xl">
+                Usage &amp; implementation of our software
+              </h4>
+            </div>
+          </div>
+          <div className="flex gap-6 items-center">
+            <img src="icon-chart.svg" alt="chart-icon" />
+            <div className="flex flex-col">
+              <h4 className="custom-color font-bold text-xl">
+                How we help drive innovation
+              </h4>
+            </div>
+          </div>
+        </div>
+        {/*  */}
+        <div className=" p-8 rounded-lg shadow-lg w-full max-w-md mt-10">
           <form action="#" method="post" className="flex flex-col space-y-4">
             <input
               type="text"
